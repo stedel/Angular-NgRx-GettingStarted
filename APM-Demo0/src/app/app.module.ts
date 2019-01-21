6db3640cb4a0bc3ba3a291b01620c02821f7b336
stedel <steffen.meifort@gmail.com>
2019-01-21 18:07:27 +0100

    Add @ngrx/store-devtools

--- APM-Demo0/src/app/app.module.ts
+++ APM-Demo0/src/app/app.module.ts
@@ -2,7 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 
+import { environment } from 'src/environments/environment';
+
+// NgRx
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 // Imports for loading & configuring the in-memory web api
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
@@ -27,6 +31,7 @@ import { UserModule } from './user/user.module';
     UserModule,
     AppRoutingModule,
     StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({ name: 'APM Demo App DevTools', maxAge: 25, logOnly: environment.production }),
   ],
   declarations: [AppComponent, ShellComponent, MenuComponent, WelcomeComponent, PageNotFoundComponent],
   bootstrap: [AppComponent],
