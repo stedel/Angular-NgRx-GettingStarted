381298bb4673a34e6b872940d2073e8a573c4c64
stedel <steffen.meifort@gmail.com>
2019-02-11 14:11:50 +0100

    Refactor state to save current product id instead of a copy

--- APM/src/app/products/state/product.reducer.ts
+++ APM/src/app/products/state/product.reducer.ts
@@ -9,14 +9,14 @@ export interface State extends fromRoot.State {
 
 export interface ProductState {
   showProductCode: boolean;
-  currentProduct: Product;
+  currentProductId: number | null;
   products: Product[];
   error: string;
 }
 
 const initialState: ProductState = {
   showProductCode: true,
-  currentProduct: null,
+  currentProductId: null,
   products: [],
   error: '',
 };
@@ -28,9 +28,27 @@ export const getShowProductCode = createSelector(
   state => state.showProductCode,
 );
 
+export const getCurrentProductId = createSelector(
+  getProductFeatureState,
+  state => state.currentProductId,
+);
+
 export const getCurrentProduct = createSelector(
   getProductFeatureState,
-  state => state.currentProduct,
+  getCurrentProductId,
+  (state, currentProductId) => {
+    if (currentProductId === 0) {
+      return {
+        id: 0,
+        productName: '',
+        productCode: 'New',
+        description: '',
+        starRating: 0,
+      };
+    } else {
+      return currentProductId ? state.products.find(p => p.id === currentProductId) : null;
+    }
+  },
 );
 
 export const getProducts = createSelector(
@@ -49,21 +67,15 @@ export function productReducer(state = initialState, action: ProductActions): Pr
       return { ...state, showProductCode: action.payload };
 
     case ProductActionTypes.SetCurrentProduct:
-      return { ...state, currentProduct: { ...action.payload } };
+      return { ...state, currentProductId: action.payload.id };
 
     case ProductActionTypes.ClearCurrentProduct:
-      return { ...state, currentProduct: null };
+      return { ...state, currentProductId: null };
 
     case ProductActionTypes.InitializeCurrentProduct:
       return {
         ...state,
-        currentProduct: {
-          id: 0,
-          productName: '',
-          productCode: 'New',
-          description: '',
-          starRating: 0,
-        },
+        currentProductId: 0,
       };
 
     case ProductActionTypes.LoadSuccess:
